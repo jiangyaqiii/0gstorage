b326b2ad8e779db1f1e75cb9f3bec01912f27d64
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-23 00:19:13 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,3 +4,5 @@
     cd ~/0g-storage-node/run
     screen -dmS zgs_node_session $HOME/0g-storage-node/target/release/zgs_node --config $HOME/0g-storage-node/run/config.toml
     echo '====================== 重启完成 ==========================='
+    cd
+    rm restart.sh
